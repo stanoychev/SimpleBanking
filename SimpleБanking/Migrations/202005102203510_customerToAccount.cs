124241f0e4eaaff7f057df2de84f7a0c810ee957
dsfs sfsd <staneutschev1@gmail.com>
2020-05-11 01:06:28 +0300

    cutomer to account link

---
+++ SimpleБanking/Migrations/202005102203510_customerToAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleBanking.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class customerToAccount : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Account_Id", c => c.Int());
+            CreateIndex("dbo.Customers", "Account_Id");
+            AddForeignKey("dbo.Customers", "Account_Id", "dbo.Accounts", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "Account_Id", "dbo.Accounts");
+            DropIndex("dbo.Customers", new[] { "Account_Id" });
+            DropColumn("dbo.Customers", "Account_Id");
+        }
+    }
+}
