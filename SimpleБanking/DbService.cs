1dbb8c606634b621ecbd072e7865a9ea6e0b4ca3
dsfs sfsd <staneutschev1@gmail.com>
2020-05-13 04:53:23 +0300

    a

--- SimpleБanking/DbService.cs
+++ SimpleБanking/DbService.cs
@@ -14,149 +14,151 @@ namespace SimpleBanking
         string GetFormatedHistory((string user, string pin) credentials);
     }
 
-    public class DbService : IDbService
-    {
-        public bool CustomerExists((string user, string pin) credentials)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool CustomerExists(string user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Deposit((string user, string pin) credentials, double amount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public double? GetBalance((string user, string pin) credentials)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFormatedHistory((string user, string pin) credentials)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Transfter((string user, string pin) credentials, double amount, string user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Withdraw((string user, string pin) credentials, double amount)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     //public class DbService : IDbService
     //{
-    //    readonly IBankDb bankDb;
-    //    readonly Tools tools = new Tools();
+    //    public bool CustomerExists((string user, string pin) credentials)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-    //    public DbService(IBankDb bankDb_) => bankDb = bankDb_;
+    //    public bool CustomerExists(string user)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
     //    public bool Deposit((string user, string pin) credentials, double amount)
     //    {
-    //        var customer = GetCustomer(credentials);
-    //        if (customer == null)
-    //            return false;
-
-    //        AddTransaction(amount, null, customer);
-
-    //        var numberOfSavedItems = bankDb.SaveChanges();
-    //        return numberOfSavedItems == 2;
+    //        throw new NotImplementedException();
     //    }
 
     //    public double? GetBalance((string user, string pin) credentials)
     //    {
-    //        var customer = GetCustomer(credentials);
-    //        if (customer == null)
-    //            return null;
-
-    //        return customer.Transactions
-    //            .Select(x => x.From.CustomerId == customer.CustomerId ? -x.Amount : x.Amount).Sum();
+    //        throw new NotImplementedException();
     //    }
 
     //    public string GetFormatedHistory((string user, string pin) credentials)
     //    {
-    //        var customer = GetCustomer(credentials);
-    //        if (customer == null)
-    //            return $"[{credentials.user}] not found.";
-
-    //        return $"History for [{credentials.user}]:" +
-    //            string.Join("\n", customer.Transactions
-    //            .OrderByDescending(x => x.Date)
-    //            .ToList()
-    //            .Select(transaction => 
-    //                    "[Date" + transaction.Date.Value.ToString("hh:mm:ss dd/MM/yyyy") + "]" + 
-    //                    transaction.From == null ?
-    //                    "[Deposit]" : transaction.To == null ?
-    //                    "[Withdraw]" :
-    //                    $"[Transaction from [{transaction.From.Name}] to [{transaction.To.Name}]]" +
-    //                    $"[Amount {string.Format("{0:0.00}", transaction.Amount)}]"));
+    //        throw new NotImplementedException();
     //    }
 
     //    public bool Transfter((string user, string pin) credentials, double amount, string user)
     //    {
-    //        var customer = GetCustomer(credentials);
-    //        if (customer == null)
-    //            return false;
-
-    //        var recipient = GetCustomer(user);
-    //        if (recipient == null)
-    //            return false;
-
-    //        AddTransaction(amount, customer, recipient);
-
-    //        var numberOfSavedItems = bankDb.SaveChanges();
-    //        return numberOfSavedItems == 5;
+    //        throw new NotImplementedException();
     //    }
-
-    //    public bool CustomerExists((string user, string pin) credentials) => GetCustomer(credentials) != null;
-
-    //    public bool CustomerExists(string user) => GetCustomer(user) != null;
 
     //    public bool Withdraw((string user, string pin) credentials, double amount)
     //    {
-    //        var customer = GetCustomer(credentials);
-    //        if (customer == null)
-    //            return false;
-
-    //        AddTransaction(amount, customer, null);
-
-    //        var numberOfSavedItems = bankDb.SaveChanges();
-    //        return numberOfSavedItems == 3;
-    //    }
-
-    //    private void AddTransaction(double amount, Customer from, Customer to)
-    //        => bankDb.Transactions.Add(new Transaction()
-    //        {
-    //            Amount = amount,
-    //            Date = DateTime.Now,
-    //            From = from,
-    //            To = to
-    //        });
-
-    //    private Customer GetCustomer((string user, string pin) credentials)
-    //    {
-    //        var hashedUser = tools.HashString(credentials.user);
-    //        var hashedPin = tools.HashString(credentials.pin);
-
-    //        var customer = bankDb.Customers
-    //            .FirstOrDefault(x => string.Equals(x.User, hashedUser));
-
-    //        return string.Equals(customer.Pin, hashedPin) ? customer : null;
-    //    }
-
-    //    private Customer GetCustomer(string user)
-    //    {
-    //        var hashedUser = tools.HashString(user);
-
-    //        return bankDb.Customers.FirstOrDefault(x => string.Equals(x.User, hashedUser));
+    //        throw new NotImplementedException();
     //    }
     //}
+
+    public class DbService : IDbService
+    {
+        readonly IBankDb bankDb;
+        readonly Tools tools = new Tools();
+
+        public DbService(IBankDb bankDb_) => bankDb = bankDb_;
+
+        public bool Deposit((string user, string pin) credentials, double amount)
+        {
+            var customer = GetCustomer(credentials);
+            if (customer == null)
+                return false;
+
+            AddTransaction(customer, amount, null, customer);
+
+            var numberOfSavedItems = bankDb.SaveChanges();
+            return numberOfSavedItems == 3;
+        }
+
+        public double? GetBalance((string user, string pin) credentials)
+        {
+            var customer = GetCustomer(credentials);
+            if (customer == null)
+                return null;
+            throw new Exception();
+            return customer.Transactions
+                .Select(transaction => transaction.From == null ? transaction.Amount : -transaction.Amount).Sum();
+        }
+
+        public string GetFormatedHistory((string user, string pin) credentials)
+        {
+            throw new Exception();
+            var customer = GetCustomer(credentials);
+            if (customer == null)
+                return $"[{credentials.user}] not found.";
+
+            return $"History for [{credentials.user}]:" +
+                string.Join("\n", customer.Transactions
+                .OrderByDescending(x => x.Date)
+                .ToList()
+                .Select(transaction =>
+                        "[Date" + transaction.Date.Value.ToString("hh:mm:ss dd/MM/yyyy") + "]" +
+                        transaction.From == null ?
+                        "[Deposit]" : transaction.To == null ?
+                        "[Withdraw]" :
+                        $"[Transaction from [{transaction.From.Name}] to [{transaction.To.Name}]]" +
+                        $"[Amount {string.Format("{0:0.00}", transaction.Amount)}]"));
+        }
+
+        public bool Transfter((string user, string pin) credentials, double amount, string user)
+        {
+            var customer = GetCustomer(credentials);
+            if (customer == null)
+                return false;
+
+            var recipient = GetCustomer(user);
+            if (recipient == null)
+                return false;
+
+            AddTransaction(customer, amount, customer, recipient);
+
+            var numberOfSavedItems = bankDb.SaveChanges();
+            return numberOfSavedItems == 4;
+        }
+
+        public bool CustomerExists((string user, string pin) credentials) => GetCustomer(credentials) != null;
+
+        public bool CustomerExists(string user) => GetCustomer(user) != null;
+
+        public bool Withdraw((string user, string pin) credentials, double amount)
+        {
+            var customer = GetCustomer(credentials);
+            if (customer == null)
+                return false;
+
+            AddTransaction(customer, amount, customer, null);
+
+            var numberOfSavedItems = bankDb.SaveChanges();
+            return numberOfSavedItems == 3;
+        }
+
+        private void AddTransaction(Customer customer, double amount, Customer from, Customer to)
+            => bankDb.Customers.First(x => x.CustomerId == customer.CustomerId)
+            .Transactions.Add(new Transaction()
+            {
+                Amount = amount,
+                Date = DateTime.Now,
+                From = from,
+                To = to
+            });
+
+        private Customer GetCustomer((string user, string pin) credentials)
+        {
+            var hashedUser = tools.HashString(credentials.user);
+            var hashedPin = tools.HashString(credentials.pin);
+
+            var customer = bankDb.Customers
+                .FirstOrDefault(x => string.Equals(x.User, hashedUser));
+
+            return string.Equals(customer.Pin, hashedPin) ? customer : null;
+        }
+
+        private Customer GetCustomer(string user)
+        {
+            var hashedUser = tools.HashString(user);
+
+            return bankDb.Customers.FirstOrDefault(x => string.Equals(x.User, hashedUser));
+        }
+    }
 }
